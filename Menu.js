da37998e161cc15cf8a99ba09232a924b176c1b8
Nicbudd <niczippy77@gmail.com>
2017-11-03 23:14:44 -0400

    *notices your javascript* OwO What's this?

---
+++ Menu.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    var node = document.createElement("LI");
+    var textnode = document.createTextNode("Milk");
+    node.appendChild(textnode);
+    document.getElementById("Menu").appendChild(node);
+}
