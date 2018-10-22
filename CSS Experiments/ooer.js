1d51ae106d6801e89a816df7c6e9ede2c20c237f
Nicbudd <niczippy77@gmail.com>
2018-10-22 11:26:14 -0400

    yay

--- CSS Experiments/ooer.js
+++ CSS Experiments/ooer.js
@@ -1,4 +1,5 @@
 function addShake(){
-  shake = shake + 0.1
-  document.getElementsByTagName(name).style.
+  shake = shake + 0.1;
+  document.getElementsByTagName(p).style.animation-duration = shake;
+  document.getElementById('pee').innerHTML = shake;
 }
