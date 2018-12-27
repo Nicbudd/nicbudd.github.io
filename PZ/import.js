b8fc5b5a8b98c5aa4a5270a2120355d2d5dfeec7
Nicbudd <niczippy77@gmail.com>
2018-12-27 17:02:13 -0500

    yep

--- PZ/import.js
+++ PZ/import.js
@@ -1,3 +1,3 @@
 $(document).ready(function(){
-  $('head').append('<meta name="viewport" content="width=device-width, initial-scale=1.0">');
+  $('head').append('<meta name="viewport" content="width=device-width, initial-scale=1.0"><link rel="icon" href="favicon.png" type="image/png" sizes="16x16">');
 });
