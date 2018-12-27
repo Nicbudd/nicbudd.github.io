eea16f821aa5d604a0dc6e0e9ae421f414f8b443
Nicbudd <niczippy77@gmail.com>
2018-12-27 17:11:41 -0500

    k

---
+++ PZ/openPhotoDetails.js
@@ -0,0 +1,7 @@
+function openDetails(detailsName){
+	document.getElementById(detailsName).style.visibility = 'visible';
+}
+
+function hideDetails(detailsName){
+	document.getElementById(detailsName).style.visibility = "hidden";
+}
