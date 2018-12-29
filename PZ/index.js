fc018e74e7f69e46884e73bbce4c46db9c200ac9
Nicbudd <niczippy77@gmail.com>
2018-12-29 13:21:56 -0500

    yay its nice

--- PZ/index.js
+++ PZ/index.js
@@ -1,25 +1,28 @@
 function openDetails(detailsName){
+	document.getElementById(detailsName).style.opacity = 1;
 	document.getElementById(detailsName).style.visibility = 'visible';
+
 }
 
 function hideDetails(detailsName){
-	document.getElementById(detailsName).style.visibility = "hidden";
+	document.getElementById(detailsName).style.opacity = 0;
+	setTimeout(turnVisibilityOff, 500, detailsName);
 }
 
 var menuVisible = false
 
-function showMenu(){
+function showMenu(itemName){
 	if (menuVisible === true){
 		document.getElementById('menu').style.opacity = 0;
-		menuVisible = false
-		setTimeout(turnVisibilityOff, 500)
+		menuVisible = false;
+		setTimeout(turnVisibilityOff('menu'), 500);
 	} else {
 		document.getElementById('menu').style.opacity = 1;
 		document.getElementById('menu').style.visibility = "visible";
-		menuVisible = true
+		menuVisible = true;
 	}
 }
 
-function turnVisibilityOff() {
-	document.getElementById('menu').style.visibility = "hidden";
+function turnVisibilityOff(elementName) {
+	document.getElementById(elementName).style.visibility = "hidden";
 }
