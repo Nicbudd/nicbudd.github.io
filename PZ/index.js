c758e824c2ce23705fab913a3216b591f9912a2d
Nicbudd <niczippy77@gmail.com>
2019-02-26 20:02:47 -0500

    sdfsdf

--- PZ/index.js
+++ PZ/index.js
@@ -1,28 +1,51 @@
+function turnVisibilityOff(elementName) {
+	document.getElementById(elementName).style.visibility = "hidden";
+};
+
 function openDetails(detailsName){
 	document.getElementById(detailsName).style.opacity = 1;
 	document.getElementById(detailsName).style.visibility = 'visible';
 
-}
+};
 
 function hideDetails(detailsName){
 	document.getElementById(detailsName).style.opacity = 0;
 	setTimeout(turnVisibilityOff, 500, detailsName);
-}
+};
 
-var menuVisible = false
+var menuVisible = false;
 
-function showMenu(itemName){
+function showMenu(){
 	if (menuVisible === true){
-		document.getElementById('menu').style.opacity = 0;
+		menu = document.getElementById('menu')
+		menu.style.opacity = 0;
+		setTimeout(turnVisibilityOff, 500, 'menu');
 		menuVisible = false;
-		setTimeout(turnVisibilityOff('menu'), 500);
 	} else {
 		document.getElementById('menu').style.opacity = 1;
 		document.getElementById('menu').style.visibility = "visible";
 		menuVisible = true;
-	}
+	};
+};
+
+
+function stopMoving(){
+	document.getElementById('zerbTitleAnim').style.width = "50%"
+	document.getElementById('pangTitleAnim').style = "width:50%!important;"
+	document.getElementById('zerbTitleAnim').style.left = "50%"
+	document.getElementById('pangTitleAnim').style.right = "50%"
 }
 
-function turnVisibilityOff(elementName) {
-	document.getElementById(elementName).style.visibility = "hidden";
+function load(){
+	
+	document.getElementById('zerbTitleAnim').onmouseover = stopMoving();
+	document.getElementById('pangTitleAnim').onmouseover = stopMoving();
+	
+	setTimeout(function(){
+		document.getElementById('zerbTitleAnim').style = ""
+		document.getElementById('pangTitleAnim').style = ""
+	}, 2500);
 }
+
+document.addEventListener("DOMContentLoaded", load);
+
