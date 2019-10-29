2c156011ada6608fe5a7fc641b8ecabad7fc2115
Nicbudd <niczippy77@gmail.com>
2019-10-29 00:22:50 -0400

    juice

--- New/index.js
+++ New/index.js
@@ -1,14 +1,23 @@
 function randomColor(){
-	var colors = ["var(--red)", "var(--green)", "var(--blue)", "var(--black)"]
+	var colors = ["red", "green", "blue", "black"]
 	return colors[Math.floor(Math.random()* colors.length)]
 }
 
 function conditionedColor(prevColors){
 	
 	var randColor;
+	
+	
 	do{
+		
+		//generate random color
 		randColor = randomColor();
 		var generateNew = false;
+		
+		//first color can't be black
+		if(prevColors.length == 0 && randColor == "black"){
+			generateNew = true;
+		};
 		
 		//regenerate if same as previous 2 colors
 		if (randColor == prevColors[prevColors.length - 1] || randColor == prevColors[prevColors.length - 2]){
@@ -22,6 +31,9 @@ function conditionedColor(prevColors){
 		
 	}
 	while (generateNew == true);
+	//regenerates random color if any condition is met
+	
+	
 	return randColor
 	
 }
@@ -29,48 +41,87 @@ function conditionedColor(prevColors){
 
 
 
-function bodyLoad(){
+function bodyLoad(page){
 	
+	
+	//color all of the body headings
 	var mainBody = document.getElementById("mainBody");
-	var prevColors = [];
+	
+	var headingPrevColors = [];
 	var h1Elem = mainBody.getElementsByTagName("h1");
 	var h2Elem = mainBody.getElementsByTagName("h2");
 	var h3Elem = mainBody.getElementsByTagName("h3");
 	var h4Elem = mainBody.getElementsByTagName("h4");
 	
 	for (let i = 0; i < h1Elem.length; i++){
-		var color = conditionedColor(prevColors);
-		h1Elem[i].style.background = color;
-		prevColors.push(color);
+		var color = conditionedColor(headingPrevColors);
+		h1Elem[i].setAttribute("class", color);
+		headingPrevColors.push(color);
 	}
 	
 	for (let i = 0; i < h2Elem.length; i++){
-		var color = conditionedColor(prevColors);
-		h2Elem[i].style.background = color;
-		prevColors.push(color);
+		var color = conditionedColor(headingPrevColors);
+		h2Elem[i].setAttribute("class", color);
+		headingPrevColors.push(color);
 	}
 	
 	for (let i = 0; i < h3Elem.length; i++){
-		var color = conditionedColor(prevColors);
-		h3Elem[i].style.background = color;
-		prevColors.push(color);
+		var color = conditionedColor(headingPrevColors);
+		h3Elem[i].setAttribute("class", color);
+		headingPrevColors.push(color);
 	}
 	
 	for (let i = 0; i < h4Elem.length; i++){
-		var color = conditionedColor(prevColors);
-		h4Elem[i].style.background = color;
-		prevColors.push(color);
+		var color = conditionedColor(headingPrevColors);
+		h4Elem[i].setAttribute("class", color);
+		headingPrevColors.push(color);
 	}
+	
+	//load sideBar content
+	
+	var sideBarContent = [
+	"<h1>Contact Me</h1><p>Got any questions? Need help? Wanna report an issue? Here's how to get in touch:</p><ul><li>Professional Emails, Website Related: <a href='mailto:nhh8629@wmich.edu'>nhh8629@wmich.edu</a></li><li>Personal Messages: <a href='mailto:niczippy77@gmail.com'>niczippy77@gmail.com</a></li></ul>",
+	];
+	
+	var sideBarPrevColors = [];
+	var sideBar = document.getElementById("sideBar");
+	
+	for (let i = 0; i < sideBarContent.length; i++){
+		
+		var sideBarObj = document.createElement("div")
+		
+		sideBarObj.innerHTML = sideBarContent[i]
+		
+		var color = conditionedColor(sideBarPrevColors);
+		sideBarObj.setAttribute("class", "sideBarContent " + color);
+		sideBarPrevColors.push(color);
+		
+		sideBar.appendChild(sideBarObj)
+		
+	};
+	
+	//load all of the other important content
+	
+	var head = document.getElementsByTagName("head")[0];
+	var body = document.getElementsByTagName("body")[0];
+	
+	var addToHead = document.createElement()
+	
+	
 	
 }
 
 //[original tab to be expanded, tab text, whether it is expanded yet or not, [children of tab]]
 var tabInfo = [
-["main", "Menu", false, ["cube"], ""], 
-["cube", "Cube", false, ["cubeHome", "algs", "comps"], ""],
+["main", "Menu", false, ["cube", "about"], ""], 
+["about", "", false, [], ""],
+["cube", "Cube", false, ["cubeHome", "algs", "comps", "events"], ""],
 ["cubeHome", "", false, [], ""], 
 ["algs", "", false, [], ""], 
 ["comps", "", false, [], ""], 
+["events", "Events", false, ["threeEvent", "otherEvent"], ""],
+["threeEvent", "", false, [], ""], 
+["otherEvent", "", false, [], ""], 
 ]
 
 function menuCollapse(tabToCollapse){
